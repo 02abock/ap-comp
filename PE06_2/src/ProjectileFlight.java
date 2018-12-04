f2fe902294e4e95d1239a1ace099fff11a98429b
02abock <43686807+02abock@users.noreply.github.com>
2018-12-04 10:51:25 -0500

    reeeeeeeeeeeeeeeeeeeeeeee

--- PE06_2/src/ProjectileFlight.java
+++ PE06_2/src/ProjectileFlight.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
 public class ProjectileFlight{
 	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Double velocity=sc.nextDouble();
 		
+		Double deltaT=0.01; 
+		Double distance=0.0;
+		Double gravity=9.81;
+		int counter=0;
+
+		while(velocity>0) {
+			
+			distance=distance+(velocity*deltaT);
+			velocity=velocity-(gravity*deltaT);
+	
+			counter+=1;
+		
+			if(counter==100) {
+				System.out.println(distance);
+				counter=0;
+			}
+		}
 	
 	}
 }	
