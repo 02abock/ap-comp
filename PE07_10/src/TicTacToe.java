0ff0a83386c86647582903330e0fe6eba10e8670
02abock <43686807+02abock@users.noreply.github.com>
2018-12-14 10:50:23 -0500

    ree

--- PE07_10/src/TicTacToe.java
+++ PE07_10/src/TicTacToe.java
@@ -8,5 +8,27 @@ public class TicTacToe{
 			}
 		}
 	}
+public void set(int i, int j, String player) {
+	if(board[i][j].equals(" ")) {
+		board[i][j]=player;
+	}
 	
 }
+public String toString() {
+	String r ="";
+	for (int i =0; i< ROWS; i++) {
+		r=r+"|";
+		for(int j =0; j<COLUMNS;j++) {
+			r=r+board[i][j];
+			r=r+"|\n";
+		}
+		
+				
+	}
+	return r;
+}
+private String[][] board;
+private static final int ROWS=3;
+private static final int COLUMNS=3;
+
+}        
---
+++ PE07_10/src/TicTacToeRunner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TicTacToeRunner{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String player="X";
+		TicTacToe game=new TicTacToe();
+		boolean done = false;
+		while(!done)
+		{
+			System.out.print(game.toString());
+			System.out.print
+		}
+	}
+}
