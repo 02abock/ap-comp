f4f25816af15c5047eea42edfd4266df9e188c99
02abock <43686807+02abock@users.noreply.github.com>
2019-01-23 11:52:47 -0500

    help

--- PE08_7-9/src/GeometryDriver.java
+++ PE08_7-9/src/GeometryDriver.java
@@ -3,8 +3,16 @@ import java.awt.geom.Point2D;
 public class GeometryDriver{
 	public static void main(String []args){
 		Ellipse2D.Double e =new Ellipse2D.Double(0.0,0.0,5.0,10.0);
-		System.out.println(e.getHeight());
+		Point2D.Double p =new Point2D.Double(0.0,0.0);
+				Point2D.Double q =new Point2D.Double(1.0,2.0);
+
 		System.out.println(Geometry.perimeter(e));
+		System.out.println(Geometry.area(e));
+		System.out.println(Geometry.angle(p,q));
+		System.out.println(Geometry.slope(p,q));
+		System.out.println(Geometry.isInside(p,e));
+
+
 	}
 
 }
